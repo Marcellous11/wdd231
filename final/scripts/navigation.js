5ea03727f6aab3f90b81e9ad1724e46ca8de8712
Marcellous Curtis Jr <1mcj001@gmail.com>
2026-02-03 02:32:08 -0600

    site plan

---
+++ final/scripts/navigation.js
@@ -0,0 +1,8 @@
+const navBtn = document.querySelector("#ham-btn")
+const navBar = document.querySelector("#nav-bar")
+
+navBtn.addEventListener("click", ()=>{
+    navBtn.classList.toggle("show")
+    navBar.classList.toggle("show")
+})
+
